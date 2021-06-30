1c4ecdc410e53e229c5257e53e2cbadb18c82d84
Hongbeom Ju <wowow1859@naver.com>
2021-06-30 23:26:18 +0900

    init

---
+++ src/test/java/com/graphqlstudy/GraphqlStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graphqlstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
